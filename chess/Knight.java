266a82d76b8a71c129e9a27fde00a829f168d03e
Kelvin Bian <kb1163@scarletmail.rutgers.edu>
2023-10-12 18:18:27 -0400

    Fixed Queen and Knight class
    used Jessica's PieceUtility methods to rewrite and fix queen and knight classes

--- chess/Knight.java
+++ chess/Knight.java
@@ -6,8 +6,16 @@ import java.lang.Math;
 import chess.ReturnPiece.PieceFile;
 
 public class Knight {
-    public static boolean checkLegal (PieceFile startFile, PieceFile endFile, int startRank, int endRank, ArrayList<ReturnPiece> piecesOnBoard) {
+
+    // return boolean signifying whether a Knight move is legal or not
+    public static boolean checkLegal (Position end, ReturnPiece n, ArrayList<ReturnPiece> piecesOnBoard) {
+        PieceFile startFile = n.pieceFile;
+        PieceFile endFile = end.f();
+        int startRank = n.pieceRank;
+        int endRank = end.r();
         ReturnPiece piece = null;
+        // loop may not be necessary for current implementation because ReturnPiece n is provided
+        // (this method originally took starting square instead of a ReturnPiece as arguments)
         for (ReturnPiece cur : piecesOnBoard) {
             if(startFile == cur.pieceFile && startRank == cur.pieceRank) {
                 piece = cur;
@@ -17,15 +25,20 @@ public class Knight {
         if (piece != null) {
             return false;
         }
+
+        // Check if own piece is on target square
+        for (ReturnPiece current : piecesOnBoard) {
+            if (Position.sameSquare(current, end) && PieceUtility.sameSide(current, n)) {return false;} 
+        }
+
+        // Check to see if target square follows Knight movement pattern
         int fileOne = Character.getNumericValue(startFile.toString().charAt(0));
         int fileTwo = Character.getNumericValue(endFile.toString().charAt(0));
 
         int fileDiff = Math.abs(fileOne - fileTwo);
         int rankDiff = Math.abs(startRank - endRank);
 
-        if (fileDiff > 0 && rankDiff > 0 && fileDiff + rankDiff == 3) {
-            return true;
-        }
+        if (fileDiff > 0 && rankDiff > 0 && fileDiff + rankDiff == 3) {return true;}
 
         return false;
     }
