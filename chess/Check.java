6d1eaa5e5979926def8d529ffdfff34f3bb91868
Kelvin Bian <kb1163@scarletmail.rutgers.edu>
2023-10-13 18:43:12 -0400

    created checkmate methods
    created checkmate methods in the Check class, one for each color

--- chess/Check.java
+++ chess/Check.java
@@ -2,6 +2,8 @@ package chess;
 
 import java.util.ArrayList;
 
+import chess.ReturnPiece.PieceFile;
+
 public class Check {
     public static boolean whiteCheck (ArrayList<ReturnPiece> piecesOnBoard) {
         Position whiteKingPosition = null;
@@ -41,5 +43,54 @@ public class Check {
             }
         }
         return false;
+    }
+
+    //see if white won (checkmated the black king)
+    public static boolean blackCheckmate (ArrayList<ReturnPiece> piecesOnBoard) {
+        // for each black piece on board, see if there is a legal move to stop the check
+        for (int i = 0; i < piecesOnBoard.size(); i++) {
+            ReturnPiece cur = piecesOnBoard.get(i);
+            if(!PieceUtility.isWhite(cur)) {
+                for (int rank = 1; rank < 9; rank++) {
+                    for (PieceFile file : PieceFile.values()) {
+                        if (legalFunction.checkLegal(new Position(rank, file), cur, piecesOnBoard)) {
+                            ArrayList<ReturnPiece> tempBoard = new ArrayList<>(piecesOnBoard);
+                            tempBoard.remove(i);
+                            ReturnPiece tempPiece = new ReturnPiece();
+                            tempPiece.pieceRank = rank;
+                            tempPiece.pieceFile = file;
+                            tempBoard.add(i, tempPiece);
+                            if (!blackCheck(tempBoard)) return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+
+    //see if black won (checkmated the white king)
+    public static boolean whiteCheckmate (ArrayList<ReturnPiece> piecesOnBoard) {
+        // for each black piece on board, see if there is a legal move to stop the check
+        for (int i = 0; i < piecesOnBoard.size(); i++) {
+            ReturnPiece cur = piecesOnBoard.get(i);
+            if(PieceUtility.isWhite(cur)) {
+                for (int rank = 1; rank < 9; rank++) {
+                    for (PieceFile file : PieceFile.values()) {
+                        if (legalFunction.checkLegal(new Position(rank, file), cur, piecesOnBoard)) {
+                            ArrayList<ReturnPiece> tempBoard = new ArrayList<>(piecesOnBoard);
+                            tempBoard.remove(i);
+                            ReturnPiece tempPiece = new ReturnPiece();
+                            tempPiece.pieceRank = rank;
+                            tempPiece.pieceFile = file;
+                            tempBoard.add(i, tempPiece);
+                            if (!whiteCheck(tempBoard)) return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
